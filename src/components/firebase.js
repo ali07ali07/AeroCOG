11f587aa22f3a5ec5226c62c82a961eae2aaba14
Syed Ali <syedyusufali6626@gmail.com>
2024-11-09 19:07:17 +0530

    added and secured all api keys in .env.loacl file

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHNx4TaNjtc0lTdzfa3qbODsfDRQo4-Vs",
-  authDomain: "aerocog-in.firebaseapp.com",
-  projectId: "aerocog-in",
-  storageBucket: "aerocog-in.firebasestorage.app",
-  messagingSenderId: "149748266667",
-  appId: "1:149748266667:web:29a77533fd7fe5e8f1a9ee",
-  measurementId: "G-0PJSDFRF9B"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
